fb378e540464f1352e0583354327d5137b551063
James Beardsley <beards18@gmail.com>
2016-04-04 23:11:54 -0400

    took out console.log() commands
    don't really need to see the unit output every time i reload the page
    anymore

--- javascripts/test.js
+++ javascripts/test.js
@@ -1,17 +1,10 @@
 var spearmen = new Unit(humanUnits.spearmen);
-console.log(spearmen);
 var archers = new Unit(humanUnits.archers);
-console.log(archers);
 var horsemen = new Unit(humanUnits.horsemen);
-console.log(horsemen);
 var swordsmen = new Unit(humanUnits.swordsmen);
-console.log(swordsmen);
 var fusiliers = new Unit(humanUnits.fusiliers);
-console.log(fusiliers);
 var knights = new Unit(humanUnits.knights);
-console.log(knights);
 var mage = new Unit(humanUnits.mage);
-console.log(mage)
 
 document.write(renderUnitCard(spearmen));
 document.write(renderUnitCard(archers));
