6f83c771d26990b81d257fcd3214f564ceb0b365
James Beardsley <beards18@gmail.com>
2016-04-06 01:29:55 -0400

    fixing space math
    so there was a problem with the previous check adjacency function in
    that it did not actually return adjacent spaces, instead following some
    rude math of its own. also added a check-movability function to keep
    anyone from stepping onto mountains. sorry, human spearmen, no mountain
    climbing for you.

--- javascripts/Space.js
+++ javascripts/Space.js
@@ -9,11 +9,20 @@ var Space = function(x, y, terrain){
 }
 
 Space.prototype.render = function() {
-	var outputHTML = "<div class='gridSpace row" + this.y + " column" + this.x;
+	var outputHTML = "<div class='gridSpace row" + this.y + " col" + this.x + " terrain-" + this.terrain.name.toLowerCase();
 	if (this.x % 2 != 0) {
 		outputHTML += " evenSpace";
 	}
-	outputHTML += "'><div class='coord'>" + this.x + ", " + this.y + "</div>";
+	if (this.activeSpace) {
+		outputHTML += " activeSpace"
+	}
+	if (this.moveable) {
+		outputHTML += " movable"
+	}
+	if (this.targetable) {
+		ouputHTML += " targetable"
+	}
+	outputHTML += "'><div class='coord'>" + this.x + ", " + this.y + " " + this.terrain.name + "</div>";
 	if (this.contains) {
 		outputHTML += this.contains.render()
 	}
@@ -22,17 +31,36 @@ Space.prototype.render = function() {
 }
 
 Space.prototype.isAdjacentTo = function(space){
-	if (this.x == space.x || this.x == space.x-1 || this.x == space.x+1) {
-		if (this.x % 2 == 0) {
-			if (this.y == space.y-1 || this.y == space.y) {
+	if (this.x == space.x) {
+		if (this.y == space.y - 1 || this.y == space.y + 1) {
+			return true
+		}
+	}
+	
+	if (this.x == space.x - 1 || this.x == space.x + 1) {
+		if (this.x % 2 != 0) {
+			if (this.y == space.y || this.y == space.y + 1) {
 				return true
 			}
 		} else {
-			if (this.y == space.y || this.y == space.y+1) {
+			if (this.y == space.y || this.y == space.y - 1) {
 				return true
 			}
 		}
 	}
 	
 	return false
+}
+
+Space.prototype.canMoveTo = function(space) {
+	if (space.terrain.name == "Mountains") {
+		return false
+	}
+	if (space.contains) {
+		return false
+	}
+	if (!this.isAdjacentTo(space)) {
+		return false
+	}
+	return true
 }
