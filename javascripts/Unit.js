9edbb75f1232598ac9620e583f3f7f7639a43f4b
James Beardsley <beards18@gmail.com>
2016-05-04 21:00:46 -0400

    Unit.js changes
    changed the order of the render function so it'll add the unit graphic
    under the inactive graphic, also outputs any active tokens

--- javascripts/Unit.js
+++ javascripts/Unit.js
@@ -18,6 +18,18 @@ var Unit = function(unit) {
 	this.tokens = [];
 	
 	this.graphic = "./images/" + unit.race + "-" + unit.name.split(" ").join("-").toLowerCase() + ".png";
+	
+	this.hits = 0;
+	
+	this.inactive = true;
+}
+
+Unit.prototype.attacks = function(target) {
+	for (var i = 0; i < this.attack.value; i++) {
+		if (coinFlip()) {
+			target.hits += 1
+		}
+	}
 }
 
 Unit.prototype.render = function(){
@@ -27,14 +39,23 @@ Unit.prototype.render = function(){
 	}
 	outputHTML += "'>";
 	outputHTML += "<div class='unit-name'>" + this.name + "</div>";
+	outputHTML += "<img src='" + this.graphic + "' class='unit-graphic'>"
+	if (this.inactive) {
+		outputHTML += "<div class='inactive'></div>"
+	}
 	var stats = ["attack", "defense", "move", "range"];
 	for (var i = 0; i < stats.length; i++){
 		outputHTML += createStatBox(stats[i], this);
 	}
-	outputHTML += "<img src='" + this.graphic + "' class='unit-graphic'>"
 	if (this.special) {
 		outputHTML += "<div class='stat-box special-box'><img src='./images/" + this.special.toLowerCase() + ".png'></div>"
 	}
+	if (this.hits > 0) {
+		outputHTML += "<div class='tokens tokens-hits'><img src='./images/hits-" + this.hits + ".png'></div>"
+	}
+	this.tokens.forEach(function(token) {
+		outputHTML += "<div class='tokens tokens-" + token + "'><img src='./images/token-" + token + ".png'></div>"
+	});
 	outputHTML += "</div>"
 	return outputHTML
 }
@@ -69,4 +90,8 @@ var setStat = function(unit, stat) {
 		}
 	}
 	return returnStat
+}
+
+function coinFlip() {
+    return (Math.floor(Math.random() * 2) == 0);
 }
