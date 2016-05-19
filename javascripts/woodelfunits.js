38458bf09adc6ae8d09a5ff09fa25737bf33efeb
James Beardsley <beards18@gmail.com>
2016-05-19 02:32:25 -0400

    added wood elf units, no graphics yet

---
+++ javascripts/woodelfunits.js
@@ -0,0 +1,100 @@
+var woodElfUnits = {
+	"crows": {
+		"race": "woodelf",
+		"name": "Crows",
+		"level": 1,
+		"special": "Charging",
+		"attack": {
+			"value": 1
+		},
+		"move": {
+			"value": 3
+		}
+	},
+	"dryads": {
+		"race": "woodelf",
+		"name": "Dryads",
+		"level": 1,
+		"special": "Steadfast",
+		"defense": {
+			"value": 1
+		}
+	},
+	"woodelves": {
+		"race": "woodelf",
+		"name": "Wood Elves",
+		"level": 1,
+		"range": {
+			"value": 2,
+			"special": true
+		}
+	},
+	"eagleriders": {
+		"race": "woodelf",
+		"name": "Eagle Riders",
+		"level": 2,
+		"special": "Piercing",
+		"range": {
+			"value": 2,
+			"special": true
+		},
+		"attack": {
+			"value": 3
+		},
+		"defense": {
+			"special": true
+		},
+		"move": {
+			"value": 3
+		}
+	},
+	"giantsnake": {
+		"race": "woodelf",
+		"name": "Giant Snake",
+		"level": 2,
+		"special": "Pinning",
+		"attack": {
+			"value": 4
+		},
+		"defense": {
+			"value": 2
+		},
+		"move": {
+			"value": 2
+		}
+	},
+	"poisonbloom": {
+		"race": "woodelf",
+		"name": "Poison Bloom",
+		"level": 2,
+		"special": "Explosive",
+		"range": {
+			"value": 3,
+			"special": true
+		},
+		"attack": {
+			"value": 3
+		},
+		"defense": {
+			"value": 2
+		},
+		"move": {
+			"value": 0
+		}
+	},
+	"elderdryad": {
+		"race": "woodelf",
+		"name": "Elder Dryad",
+		"level": 3,
+		"special": "Steadfast",
+		"attack": {
+			"value": 5
+		},
+		"defense": {
+			"value": 3
+		},
+		"move": {
+			"special": true
+		}
+	}
+}
