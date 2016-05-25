e5b49fe2b9e9327267b9c8c57e09be02ff21dbf2
James Beardsley <beards18@gmail.com>
2016-05-25 01:52:18 -0400

    side database
    this contains all the data needed to produce sample battlefields from
    some simple strings

---
+++ javascripts/armylist.js
@@ -0,0 +1,234 @@
+var armyList = {
+	"human": {
+		"attacker": {
+			"map": ["##..##",
+					"#-..-#",
+					"......",
+					"......"
+			],
+			"army": [
+				"horsemen 6 2",
+				"horsemen 6 3",
+				"spearmen 5 0",
+				"spearmen 5 5",
+				"archers 5 4",
+				"knights 5 2",
+				"knights 5 3",
+				"fusiliers 5 1",
+				"mage 6 1"
+			]
+		},
+		"defender": {
+			"map": ["^....^",
+					".-=.-.",
+					"#---##",
+					"^#..#^"],
+			"army": [
+				"archers 0 2",
+				"archers 1 4",
+				"archers 1 3",
+				"spearmen 2 1",
+				"spearmen 2 4",
+				"swordsmen 2 2",
+				"swordsmen 2 3",
+				"fusiliers 1 1",
+				"mage 1 2"
+			]
+		}
+	},
+	"dwarf": {
+		"attacker": {
+			"map": [
+				"^.--.^",
+				"..##..",
+				"......",
+				"^....^"],
+			"army": [
+				"maddwarves 5 1",
+				"maddwarves 5 2",
+				"maddwarves 5 4",
+				"rifledwarves 6 3",
+				"rifledwarves 6 4",
+				"gyrocopters 6 5",
+				"gyrocopters 6 1",
+				"cannon 6 2",
+				"thane 5 3"]
+		},
+		"defender": {
+			"map": [
+				"^=--=^",
+				"^.==.^",
+				"^....^",
+				"^....^"],
+			"army": [
+				"dwarves 2 4",
+				"dwarves 1 1",
+				"dwarves 0 1",
+				"rifledwarves 0 3",
+				"rifledwarves 0 4",
+				"thanesdwarves 1 2",
+				"thanesdwarves 1 4",
+				"cannon 0 2",
+				"thane 1 3"]
+		}
+	},
+	"woodelf": {
+		"attacker": {
+			"map": [
+				"##.##-",
+				"#.##..",
+				"...#..",
+				"......"],
+			"army": [
+				"crows 5 1",
+				"crows 6 0",
+				"dryads 5 2",
+				"dryads 5 3",
+				"dryads 5 0",
+				"giantsnake 5 5",
+				"eagleriders 5 4",
+				"eagleriders 6 5",
+				"elderdryad 6 3"]
+		},
+		"defender": {
+			"map": [
+				"######",
+				"#-=-.#",
+				"#-.-.#",
+				"######"],
+			"army": [
+				"woodelves 1 1",
+				"woodelves 1 2",
+				"dryads 2 1",
+				"dryads 2 2",
+				"dryads 2 3",
+				"giantsnake 2 4",
+				"poisonbloom 1 3",
+				"eagleriders 1 4",
+				"elderdryad 2 5"]
+		}
+	},
+	"highelf": {
+		"attacker": {
+			"map": [
+				"..--..",
+				".-==-.",
+				"......",
+				"......"],
+			"army": [
+				"spearelves 5 2",
+				"spearelves 5 3",
+				"rangers 6 3",
+				"rangers 5 1",
+				"ballista 6 2",
+				"cavalry 5 0",
+				"cavalry 5 5",
+				"warsinger 6 1",
+				"mage 6 4"]
+		},
+		"defender": {
+			"map": [
+				"......",
+				"#----#",
+				"#=-=-#",
+				"..^^.."],
+			"army": [
+				"spearelves 2 1",
+				"spearelves 2 3",
+				"ballista 1 2",
+				"ballista 1 3",
+				"ballista 1 4",
+				"shiningones 2 2",
+				"shiningones 2 4",
+				"warsinger 0 2",
+				"mage 1 1"]
+		}
+	},
+	"orc": {
+		"attacker": {
+			"map": [
+				"..###.",
+				".-##-.",
+				"......",
+				"......"],
+			"army": [
+				"fighters 6 1",
+				"fighters 6 2",
+				"fighters 6 3",
+				"goblins 5 1",
+				"goblins 5 2",
+				"goblins 5 3",
+				"goblins 5 4",
+				"wolfriders 6 0",
+				"wolfriders 6 5",
+				"troll 6 4",
+				"general 7 2"]
+		},
+		"defender": {
+			"map": [
+				"^^==^^",
+				"^#---^",
+				".....^",
+				"......"],
+			"army": [
+				"fighters 2 2",
+				"fighters 2 3",
+				"fighters 2 4",
+				"goblins 2 0",
+				"goblins 2 1",
+				"goblinarchers 0 2",
+				"goblinarchers 0 3",
+				"troll 1 4",
+				"captains 1 2",
+				"captains 1 3",
+				"general 1 1"]
+		}
+	},
+	"undead": {
+		"attacker": {
+			"map": [
+				"^.....",
+				"^^..=.",
+				"^.....",
+				"......"],
+			"army": [
+				"ghosts 5 2",
+				"ghosts 5 3",
+				"ghosts 5 4",
+				"ghosts 5 5",
+				"bats 6 1",
+				"bats 7 5",
+				"bats 7 4",
+				"bats 7 3",
+				"zombies 7 2",
+				"zombies 7 1",
+				"hellhound 6 5",
+				"hellhound 6 2",
+				"catapult 6 4",
+				"necromancer 6 3"]
+		},
+		"defender": {
+			"map": [
+				"......",
+				"#....#",
+				"^#..#^",
+				"^.##.^"],
+			"army": [
+				"zombies 2 1",
+				"zombies 2 2",
+				"zombies 2 3",
+				"zombies 2 4",
+				"zombies 1 0",
+				"zombies 1 5",
+				"ghosts 1 1",
+				"ghosts 1 4",
+				"bats 1 2",
+				"bats 1 3",
+				"catapult 0 3",
+				"catapult 0 5",
+				"skeletons 0 2",
+				"necromancer 0 4"]
+		}
+	}
+}
+
